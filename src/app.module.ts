69e186765dcba45ed9e3dd2d8c14357270c1cd0e
Cong.Nguyen <cong.nguyen@ip-192-168-1-6.ap-southeast-1.compute.internal>
2023-09-30 11:12:24 +0700

    update throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { OAuthModule } from './oauth/oauth.module';
 import { ContentModule } from './content/content.module';
 import { InsightsModule } from './insights/insights.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   controllers: [],
@@ -16,6 +17,12 @@ import { InsightsModule } from './insights/insights.module';
     UserModule,
     ContentModule,
     InsightsModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
 })
 export class AppModule {}
